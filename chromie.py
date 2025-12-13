f8772c75be0735d16c1d113350d1090b70c8ed49
Nicole Thornton <nicole.thornton330@gmail.com>
2025-12-13 10:11:37 -0600

    Update chromie.py

--- chromie.py
+++ chromie.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 from pathlib import Path
 from datetime import datetime
 from zoneinfo import ZoneInfo
@@ -1740,4 +1741,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
